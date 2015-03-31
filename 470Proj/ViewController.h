8330a7fbc60f87e6fcfdcf67bde6670e174a9d91
Helen San <helensan@rhz-36-19.sonoma.edu>
2015-03-31 15:52:27 -0700

    Initial Commit

---
+++ 470Proj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  470Proj
+//
+//  Created by Helen San on 3/31/15.
+//  Copyright (c) 2015 SSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
